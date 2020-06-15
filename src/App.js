72de58fcbaf05ff0541a4046decb0f93ad6525f7
Michael <michael@truckertools.com>
2020-06-15 18:39:55 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+// npm install --save react-router-dom
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+
+//pages
+import home from './pages/home';
+import login from './pages/login';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={home}/>
+                        <Route exact path='/login' component={login}/>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App;
