aa5878826514c97c8a9e47792e59a14acb802ef7
Michael <michael@truckertools.com>
2020-07-13 20:35:20 -0400

    menuHamburger

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 //Material UI
 import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
 
 class navbar extends Component {
@@ -48,20 +50,24 @@ class navbar extends Component {
     }
     render() {
         let changeLogo = this.state.scrollPosition > 0.5 ? "none" : ""; //removes logo base on scroll position
-        let changeNav = this.state.scrollPosition > 0.5 ? 'primary' : 'transparent'; //change navbar color base on scroll position
-        let changeMenu = "" //initiate menu dropdown to resize
-        if (this.state.pageWidth < 1000) { //if window size is reduced, display logo and change menu dropdown
+        let changeNavBar = this.state.scrollPosition > 0.5 ? 'primary' : 'transparent'; //change navbar color base on scroll position
+        let changeMenu = "none"; //initiate menu to resize
+        let changeMenuItem = ""; //initiate menu items to resize
+        if (this.state.pageWidth < 1000) { //if window size is reduced, display logo and change menu
             changeLogo = "";
-            changeMenu = "none";
+            changeMenu = "";
+            changeMenuItem = "none";
         }
         return (
             <div className="navbar">
-                <AppBar position='sticky' color={changeNav} elevation={0}>
-                    <div className="nav-logo" style={{display: changeLogo}}> 
+                <AppBar position='sticky' color={changeNavBar} elevation={0}>
+                    <div className="nav-logo" style={{display:changeLogo}}> 
                         <Button color="secondary" component={Link} to="/" disableElevation="true"><h1>Everyday Eyecare</h1></Button>
                     </div>
-                    {/* <MenuIcon/> */}
-                    <div className="nav-container" style={{display: changeMenu}}>
+                    <MenuIcon color="secondary" className="menu-icon" style={{fontSize:50,display:changeMenu}}>
+                        <Button/>
+                    </MenuIcon>
+                    <div className="nav-container" style={{display:changeMenuItem}}>
                         <Button color="secondary" component={Link} to="/">Services</Button>
                         <Button color="secondary" component={Link} to="/">Eye Conditions</Button>
                         <Button color="secondary" component={Link} to="/">Appointments</Button> 
