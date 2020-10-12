0a2a005d757c48e541475c6c5d911378ce6d14e6
Michael <michael@truckertools.com>
2020-10-12 19:36:49 -0400

    headerFix

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,34 @@ import React, { Component } from 'react'
 
 class header extends Component {
     state = {
-        pathLink: window.location.pathname
+        pathLink: window.location.hash
     }
     componentDidMount() {
         window.addEventListener('click', this.listenToClick)
     }
     listenToClick = () => {
         this.setState({
-            pathLink: window.location.pathname
+            pathLink: window.location.hash
         })
         // console.log(this.state.pathLink);
     }
     switchMotto(path) {
         switch(path) {
-            case "/privacyPolicy":
+            case "#/privacyPolicy":
                 return "Privacy Policy";
-            case "/contact":
+            case "#/contact":
                 return "Contact";
-            case "/termCondition":
+            case "#/termCondition":
                 return "Terms & Conditions";
+            case "#/login":
+                return "Login";
             default:
                 return "Your Eyes. Our Passion.";
         }
     }
     render() {
-        let changeHeader = this.state.pathLink === "/" ? "550px" : "275px";
-        let changeMotto = this.state.pathLink === "/" ? "475px" : "200px";
+        let changeHeader = this.state.pathLink === "#/" ? "550px" : "275px";
+        let changeMotto = this.state.pathLink === "#/" ? "475px" : "200px";
         return (
             <div className="header" style={{height:changeHeader}}>
                 <h2 className="header-motto" style={{paddingTop:changeMotto}}>{this.switchMotto(this.state.pathLink)}</h2>
